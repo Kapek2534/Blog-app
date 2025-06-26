33f293ac3e95c576fab2999bfae7c46e423b414b
Kacper <szczyrba.kacper@o2.pl>
2025-06-26 10:46:42 +0300

    feat(ui): show loading spinner before navigating to subpages, chore(api): update backend URL to production server

--- frontend/src/components/OutletWrapper/OutletWrapper.jsx
+++ frontend/src/components/OutletWrapper/OutletWrapper.jsx
@@ -1,17 +1,41 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react";
+
+import { LightmodeContext } from "../../contexts/LightmodeContext";
+import { useContext } from "react";
 
 import styles from "./OutletWrapper.module.css";
 
 export function OutletWrapper() {
+  const navigation = useNavigation();
+  const loading = navigation.state === "loading";
+
+  const [isDark] = useContext(LightmodeContext);
+
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 25 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <Outlet />
-    </motion.div>
+    <>
+      <motion.div
+        initial={{ opacity: 0, y: 25 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {loading ? (
+          <div className={`${styles.loader} ${isDark && styles.dark}`}>
+            <ClipLoader
+              size={80}
+              color={isDark ? "#fff" : "#333"}
+              cssOverride={{
+                borderWidth: "4px",
+              }}
+            ></ClipLoader>
+            <span>Ładowanie...</span>
+          </div>
+        ) : (
+          <Outlet />
+        )}
+      </motion.div>
+    </>
   );
 }
